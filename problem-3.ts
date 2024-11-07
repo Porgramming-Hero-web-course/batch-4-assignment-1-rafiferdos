b3d19eddcc93e5b1d44c535647cfef2f6de44789
Rafi Ferdos <rafiferdos@gmail.com>
2024-11-07 14:51:48 +0600

    add countWordOccurrences function to count and log occurrences of a word in a sentence

---
+++ problem-3.ts
@@ -0,0 +1,8 @@
+const countWordOccurrences = (sentence: string, word: string): number => {
+    const sentenceArray = sentence.toLowerCase().split(' ');
+    const count = sentenceArray.filter((item) => item === word.toLowerCase()).length;
+    console.log(count);
+    return count;
+};
+
+countWordOccurrences("I love typescript", "typescript"); 
